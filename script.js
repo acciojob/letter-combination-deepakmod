872a260a706c7b1e88f86db004891f48b0e322c4
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-21 11:49:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+let keys=["0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"];
+function letterCombinations(digit) {
+  let ans=[];
+  let output="";
+  let index=0;
+  console.log( solve(digit,index,ans,output)  );
 }
+function solve(digit,index,ans,output){
+    if(index>=digit.length){
+        return ans.push(output);
+    }
 
+    let number=parseInt(digit[index]);
+    let str=keys[number];
+
+    for(let i=0;i<str.length;i++){
+        output.push(str.charAt[i]);
+        solve(digit,index+1,ans,output);
+        output.pop(str.charAt(i));
+    }
+}
 module.exports = letterCombinations;
